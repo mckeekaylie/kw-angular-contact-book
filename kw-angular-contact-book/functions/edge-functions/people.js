0f91c189b2806b78a1a5679e5f372e3ad8c8496b
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-11-06 23:49:25 -0500

    clean up people fn

--- kw-angular-contact-book/functions/edge-functions/people.js
+++ kw-angular-contact-book/functions/edge-functions/people.js
@@ -1,12 +1,6 @@
-// import people from "..people.json";
-// export default async (request, context) => {
-//   return Response.json(people);
-// };
-import * as fs from "fs";
-// /Users/kayliemckee/Documents/GitHub/kw-angular-contact-book/kw-angular-contact-book/functions/people.json
-// Calling the readFileSync() method
-// to read 'input.txt' file
-const data = fs.readFileSync("functions/people.json", {
+import { readFileSync } from "fs";
+
+const data = readFileSync("functions/people.json", {
   encoding: "utf8",
   flag: "r",
 });
