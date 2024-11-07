b2d89e8b99eb33945ef49ed1f5b5a8d7aa5b38e0
mckeekaylie <59926734+mckeekaylie@users.noreply.github.com>
2024-11-06 19:19:53 -0500

    added netlify function

--- kw-angular-contact-book/server/server.js
+++ kw-angular-contact-book/server/server.js
@@ -4,16 +4,20 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 4000;
 
-const cors = require('cors')
+const cors = require("cors");
 
 const people = require(path.join(__dirname, "./data/people.json"));
 
 app.use(cors());
 
 app.get("/api/people", function (req, res) {
+  res.json({ message: "Hello from Express!" });
+
   res.end(JSON.stringify(people, null, "    "));
 });
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+module.exports = app;
--- kw-angular-contact-book/src/shared/services/people-service.service.ts
+++ kw-angular-contact-book/src/shared/services/people-service.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { Person } from '../models/person.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PeopleServiceService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   public getPeople() {
-    return this.http.get<any>(`${environment.apiUrl}`);
+    return this.http.get<Person[]>(`${environment.apiUrl}`);
   }
 }
